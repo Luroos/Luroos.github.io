49b1291dff993ab377c60ee9a9c4140fe8e147dd
Luroos <lql2049318359@gmail.com>
2023-11-25 16:50:22 +0800

    Site updated: 2023-11-25 16:50:21

--- js/runtime.js
+++ js/runtime.js
@@ -2,10 +2,19 @@ var now = new Date();
 function createtime() {
   // 当前时间
   now.setTime(now.getTime() + 1000);
-  //var start = new Date("11/20/2023 00:00:00"); // 旅行者1号开始计算的时间
-  // var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
-  // var unit = (dis / 149600000).toFixed(6);  // 天文单位
-  var grt = new Date("01/01/2023 00:00:00");	// 时间
+  var grr = new Date("01/01/2023 00:00:00");	// 恋爱时间
+  var days = (now - grr) / 1e3 / 60 / 60 / 24,
+    dnm = Math.floor(days),
+    hours = (now - grr) / 1e3 / 60 / 60 - 24 * dnm,
+    hnum = Math.floor(hours);
+  1 == String(hnum).length && (hnum = "0" + hnum);
+  var minutes = (now - grr) / 1e3 / 60 - 1440 * dnm - 60 * hnum,
+    mnum = Math.floor(minutes);
+  1 == String(mnum).length && (mnum = "0" + mnum);
+  var seconds = (now - grr) / 1e3 - 86400 * dnm - 3600 * hnum - 60 * mnum,
+    snum = Math.round(seconds);
+  1 == String(snum).length && (snum = "0" + snum);
+  
   var grt = new Date("11/20/2023 00:00:00");	// 网站诞生时间
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
     dnum = Math.floor(days),
@@ -21,8 +30,8 @@ function createtime() {
   let currentTimeHtml = ""; 
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
-    ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold"> 和周宝在一起已经${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> 本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 </i>🚀</div>`
-    : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold"> 和周宝在一起已经${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> 本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 </i> `),
+    ? `<img class='boardsign' src='/svg/桂工-上课摸鱼啦.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold"> <i id="heartbeat" class='fas fa-heartbeat'></i> 和周宝在一起已经${dnm} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br>🚀本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 🚀</div>`
+    : `<img class='boardsign' src='/svg/桂工-下课休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold"> <i id="heartbeat" class='fas fa-heartbeat'></i> 和周宝在一起已经${dnm} 天 ${hnm} 小时 ${mnm} 分 ${snm} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br>🚀本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 🚀</div>`),
     document.getElementById("workboard") &&
     (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
